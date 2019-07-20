4302a838f0c1e2f2c439010bb982f95489c3e88c
Maximilian Taeschner <maximilian.taeschner@gmail.com>
2019-07-20 15:22:29 +0200

    feat: add post page

---
+++ pages/posts/2019-07-20-graph-databases-for-access-control.tsx
@@ -0,0 +1,48 @@
+import Post from '../../components/Post'
+
+export default function() {
+  return (
+    <Post title="Graph databases for access control">
+      <div>
+        References:
+        <ol>
+          <li>
+            <a href="https://neo4j.com/blog/access-control-lists-the-graph-database-way/">Link</a>
+          </li>
+          <li>
+            <a href="https://dl.acm.org/citation.cfm?id=3176331&dl=ACM&coll=DL</li>">Link</a>
+          </li>
+          <li>
+            <a href="https://www.fourkitchens.com/blog/article/anticipage-scalable-pagination-especially-acls/">Link</a>
+          </li>
+          <li>
+            <a href="https://www.google.com/search?q=graph+database+access+control&oq=gra&aqs=chrome.0.69i59j69i57j69i60l3j69i59.1064j0j7&sourceid=chrome&ie=UTF-8">
+              Link
+            </a>
+          </li>
+          <li>
+            <a href="https://medium.com/jettech/role-based-access-control-using-a-graph-database-2573debb685e">Link</a>
+          </li>
+          <li>
+            <a href="https://leanjavaengineering.wordpress.com/2015/04/13/attribute-based-access-control-with-a-graph-database/">
+              Link
+            </a>
+          </li>
+          <li>
+            <a href="https://csrc.nist.gov/csrc/media/publications/sp/800-162/final/documents/sp800_162_draft.pdf">
+              Link
+            </a>
+          </li>
+          <li>
+            <a href="https://neo4j.com/blog/this-week-neo4j-property-based-access-control-intro-cypher-user-path-analysis/">
+              Link
+            </a>
+          </li>
+          <li>
+            <a href="https://hackernoon.com/guys-were-doing-pagination-wrong-f6c18a91b232</li>">Link</a>
+          </li>
+        </ol>
+      </div>
+    </Post>
+  )
+}
