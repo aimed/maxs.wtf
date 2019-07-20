e1b83d24a54aec594756a48dd5d49cfc4c1414b7
Maximilian Taeschner <maximilian.taeschner@gmail.com>
2019-07-20 12:46:55 +0200

    chore: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import '../styles/index.css';
+import Head from 'next/head';
+
+function Index() {
+  return (
+    <main>
+      <Head>
+        <title>max's wtf</title>
+      </Head>
+      <h1>max's wtf</h1>
+    </main>
+  );
+}
+
+export default Index;
