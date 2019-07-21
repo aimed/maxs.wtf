ac78631cea0154317b992792480f0f756ae2e31e
Maximilian Taeschner <maximilian.taeschner@gmail.com>
2019-07-21 12:07:55 +0200

    feat: add image

--- next.config.js
+++ next.config.js
@@ -1,2 +1,5 @@
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS({})
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
+
+module.exports = withPlugins([[optimizedImages, {}], [withCSS, {}]])
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <Page title="max's wtf">
       <section className="p-2 mt-10 mb-5 relative">
+        <img src={require('../static/max.png')} alt="Max" className="w-24 absolute right-0 mr-2 bottom-0 -mb-6" />
         <h2 className="pb-0">Hi. I'm Max.</h2>
         <p>Software Engineer / guy on the internet.</p>
         <p>This is a collection of random thoughts.</p>
